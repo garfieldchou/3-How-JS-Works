f6c36c5e21ffcdee8067005bf22f9014354c59ff
shaudai <shaudai0402@hotmail.com>
2017-10-01 20:59:37 +0800

    method borrowing, mike borrows john's method to use it on mike

--- script.js
+++ script.js
@@ -84,14 +84,22 @@ var john = {
     calculateAge: function() {
         console.log(this);
         console.log(2016 - this.yearOfBirth);
-        
+        /*
         function innerFunction() {
             console.log(this); 
         }
         innerFunction();
+        */
     }
-}
+};
 
 john.calculateAge();
 
+var mike = {
+    name: 'Mike',
+    yearOfBirth: 1984
+};
+
+mike.calculateAge = john.calculateAge;
+mike.calculateAge();
 
